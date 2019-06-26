c66c2f6260e0f4c1e8d0e119e8115c6edd9c0b34
Abeeb <abibcode@gmail.com>
2019-06-26 15:47:47 +0100

    Initial commit.

---
+++ src/S3.ApiGateway/Controllers/GatewayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using S3.ApiGateway.Models;
+
+namespace S3.ApiGateway.Controllers
+{
+    [Route("")]
+    public class GatewayController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Gateway service running...");
+    }
+}
